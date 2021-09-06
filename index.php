4dd24e72a7d591006719ea37ef4eaacf20a0cda3
k21066-Shimizu <k21066kk@aitech.ac.jp>
2021-09-06 17:12:54 +0900

    perfect

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>CSS完全に理解した</title>
+	<link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<div class="origin">
+		<div class="box"></div>
+	</div>
+	<div class="text">
+		CSS<br>
+		完全に理解した
+	</div>
+</body>
